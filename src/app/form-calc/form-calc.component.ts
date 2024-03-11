03e026e486109a8755b600eecec756f398ebfb74
Mateus <mateussouza1102@gmail.com>
2024-03-11 20:45:49 -0300

    operations

--- src/app/form-calc/form-calc.component.ts
+++ src/app/form-calc/form-calc.component.ts
@@ -9,8 +9,29 @@ export class FormCalcComponent {
   num1: number = 0;
   num2: number = 0;
   sum: number = 0;
+  minus: number = 0;
+  division: number = 0;
+  multiply: number = 0;
+  poten: number = 0;
+  remnant: number = 0;
+
 
   onClickSum(){
     this.sum = this.num1 + this.num2;
+  }
+  onClickMinus(){
+    this.minus = this.num1 - this.num2;
+  }
+  onClickDivision(){
+    this.division = this.num1 / this.num2;
+  }
+  onClickMultiply(){
+    this.multiply = this.num1 * this.num2;
+  }
+  onClickPoten(){
+    this.poten = this.num1 ** this.num2;
+  }
+  onClickRemnant(){
+    this.remnant = this.num1 % this.num2;
   }
 }
